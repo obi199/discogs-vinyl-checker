49bde2814713bc0bb46df22908d64d2b71b245c4
obi199 <pebellinghausen@posteo.de>
2022-01-18 00:57:54 +0100

    update settings

--- modules/settings.py
+++ modules/settings.py
@@ -1,6 +1,6 @@
 import os
 #userdb = r"sqlite:////home/obi/dev/python/discogs-checker/db/user.db"
-userdb = r'postgres://gscikpcgepjvqm:f02461c199a6a555a6846025c2de077f7b8a6af02830a3ddf764a67f2c7281b7@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dbpltgqbgksg4t'
+userdb = r'postgresql://gscikpcgepjvqm:f02461c199a6a555a6846025c2de077f7b8a6af02830a3ddf764a67f2c7281b7@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dbpltgqbgksg4t'
 #userdb = os.getenv('DATBASE_URL')
 table = "users"
 SECRET_KEY="jDuOXpDaXjdF8"
