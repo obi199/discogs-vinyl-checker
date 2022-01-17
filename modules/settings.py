38392c761d06d125fde7d5ae4ab7f6890a33f3c2
obi199 <pebellinghausen@posteo.de>
2022-01-18 00:48:14 +0100

    settings db url addded

--- modules/settings.py
+++ modules/settings.py
@@ -1,7 +1,7 @@
 import os
 #userdb = r"sqlite:////home/obi/dev/python/discogs-checker/db/user.db"
-
-userdb = os.getenv('DATBASE_URL')
+userdb = r'postgres://gscikpcgepjvqm:f02461c199a6a555a6846025c2de077f7b8a6af02830a3ddf764a67f2c7281b7@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dbpltgqbgksg4t'
+#userdb = os.getenv('DATBASE_URL')
 table = "users"
 SECRET_KEY="jDuOXpDaXjdF8"
 user_agent="discogs_fast_checker/1.0"
