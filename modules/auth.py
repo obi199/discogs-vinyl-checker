284d3e306a8ced6b06567cd9c18bd75b8ac5c7ab
obi199 <pebellinghausen@posteo.de>
2022-01-18 01:27:00 +0100

    logger update

--- modules/auth.py
+++ modules/auth.py
@@ -14,9 +14,9 @@ import uuid
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
+# formatter = logging.Formatter(
+#         '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+#handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
